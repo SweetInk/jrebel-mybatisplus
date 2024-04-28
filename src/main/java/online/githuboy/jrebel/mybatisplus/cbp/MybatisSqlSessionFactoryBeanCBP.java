f77087b3843c3ce5079802d71e117049fef5fe6d
suchu <suchu01@hotmail.com>
2024-04-28 16:34:17 +0800

    feat Compatible with mybatis-plus 3.5.6 https://github.com/SweetInk/jrebel-mybatisplus-idea-plugin/issues/10

--- src/main/java/online/githuboy/jrebel/mybatisplus/cbp/MybatisSqlSessionFactoryBeanCBP.java
+++ src/main/java/online/githuboy/jrebel/mybatisplus/cbp/MybatisSqlSessionFactoryBeanCBP.java
@@ -49,6 +49,8 @@ public class MybatisSqlSessionFactoryBeanCBP extends JavassistClassBytecodeProce
                 public void edit(NewExpr e) throws CannotCompileException {
                     if ("org.apache.ibatis.builder.xml.XMLMapperBuilder".equals(e.getClassName())) {
                         e.replace("{ $_ = $proceed($$);  registerMapperLocationToReloader($2, __resource, $3);}");
+                    } else if ("com.baomidou.mybatisplus.core.MybatisXMLMapperBuilder".equals(e.getClassName())) {
+                        e.replace("{ $_ = $proceed($$);  registerMapperLocationToReloader($2, __resource, $3);}");
                     } else if ("com.baomidou.mybatisplus.core.MybatisXMLConfigBuilder".equals(e.getClassName())) {
                         e.replace("{ $_ = $proceed($$); configuration = (com.baomidou.mybatisplus.core.MybatisConfiguration) $_.getConfiguration(); }");
                     } else if( "com.baomidou.mybatisplus.core.MybatisConfiguration".equals(e.getClassName())){
