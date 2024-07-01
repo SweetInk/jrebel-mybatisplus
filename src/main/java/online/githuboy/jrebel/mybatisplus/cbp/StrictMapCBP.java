ed3be370833e39d2931ebff11fb028c55f0c8651
suchu <suchu01@hotmail.com>
2024-07-01 15:41:57 +0800

    feat Compatible with mybatis-plus 3.5.7 https://github.com/SweetInk/jrebel-mybatisplus-idea-plugin/issues/12

--- src/main/java/online/githuboy/jrebel/mybatisplus/cbp/StrictMapCBP.java
+++ src/main/java/online/githuboy/jrebel/mybatisplus/cbp/StrictMapCBP.java
@@ -22,12 +22,18 @@ public class StrictMapCBP extends JavassistClassBytecodeProcessor {
     @Override
     public void process(ClassPool classPool, ClassLoader classLoader, CtClass ctClass) throws Exception {
         if (!proceed) {
+            CtClass ambiguityClass = classPool.getOrNull("com.baomidou.mybatisplus.core.MybatisConfiguration$StrictMap$Ambiguity");
             ctClass.getDeclaredMethod("put").instrument(new ExprEditor() {
                 public void edit(MethodCall m) throws CannotCompileException {
                     if ("containsKey".equals(m.getMethodName())) {
                         m.replace("{  if (" + Constants.SqlMapReloaderClass + ".isReloading())    $_ = false;  else    $_ = $proceed($$);}");
                     } else if ("get".equals(m.getMethodName())) {
-                        m.replace("{  $_ = $proceed($$);  if (" + Constants.SqlMapReloaderClass + ".isReloading()       && !($_ instanceof com.baomidou.mybatisplus.core.MybatisConfiguration$StrictMap$Ambiguity))    $_ = null;}");
+                        //Before mybatis-plus version 3.5.7
+                        if (null != ambiguityClass) {
+                            m.replace("{  $_ = $proceed($$);  if (" + Constants.SqlMapReloaderClass + ".isReloading()       && !($_ instanceof com.baomidou.mybatisplus.core.MybatisConfiguration$StrictMap$Ambiguity))    $_ = null;}");
+                        } else {
+                            m.replace("{  $_ = $proceed($$);  if (" + Constants.SqlMapReloaderClass + ".isReloading()       && !($_ ==AMBIGUITY_INSTANCE))    $_ = null;}");
+                        }
                     }
                 }
             });
